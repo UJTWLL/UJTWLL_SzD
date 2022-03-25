298da4ad335380ced004e6c7e0f85df846ad3fa9
UJTWLL <79086854+UJTWLL@users.noreply.github.com>
2022-03-25 10:47:24 +0100

    Delete userRouter.js

--- Szakdolgozat/userRouter.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-module.exports = function(app) {
-    var userHandlers = require('./userController');
-    // todoList Routes
-    app.route('/tasks').post(userHandlers.loginRequired, userHandlers.profile);
-    app.route('/register').post(userHandlers.register);
-    app.route('/login').post(userHandlers.sign_in);
-};
