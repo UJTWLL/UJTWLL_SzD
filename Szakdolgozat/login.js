879254eb6361bd000cfbf8f23a144614c94e966c
UJTWLL <79086854+UJTWLL@users.noreply.github.com>
2022-03-25 11:03:25 +0100

    Add files via upload

--- Szakdolgozat/login.js
+++ Szakdolgozat/login.js
@@ -49,12 +49,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //app.use(express.static(path.join(__dirname, 'static')));
 
-//http://localhost:3000/
-app.get('/', function(req, res) {
-	// Render login template
-	res.sendFile(path.join(__dirname + '/login.html'));
-});
-
 /*app.use(function(req, res) {
   res.status(404).send({ url: req.originalUrl + ' not found' })
 });*/
@@ -95,65 +89,27 @@ app.post('/auth', function(req, res, next) {
           db.close();
       })
   });
+});
 
-  /*MongoClient.connect('mongodb+srv://jokermta:QTIq5re1999@szdcluster.4fntd.mongodb.net/test?retryWrites=true&w=majority').then((db) => {  
-    var dbo = db.db("test");
-    return dbo.collection("users").findOne(myobj, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    }).then((collection) => {
-      console.log(collection);
-      //console.log(collection);
-    }).catch(err => {
-      console.log(`DB Connection Error: ${err.message}`);
-    }).finally(() => {
-      console.log('Close DB');
-      db.close();
-    })
-    });*/
-    /*var ob = dbo.collection("users").find(myobj).toArray(
-    ).then((collection) => {
-      console.log(collection);
-    }).catch(err => {
-      console.log(`DB Connection Error: ${err.message}`);
-    }).finally(() => {
-      console.log('Close DB');
-      db.close();
-    })*/
-  //});
-
-  /*let username = req.body.username;
-	let password = req.body.password;
-  console.log("Name: "+username+", pw: "+password);
-  res.send("Name: "+username+", pw: "+password);*/
-	// Ensure the input fields exists and are not empty
-	/*if (username=="asdasd" && password=="asdasd") {
-    req.session.loggedin = true;
-    req.session.username = username;
-    res.redirect("/home");
-	}*/
-
-	/*if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
-    jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', function(err, decode) {
-      if (err) req.user = undefined;
-      req.user = decode;
-      next();
-    });
-  } else {
-    req.user = undefined;
-    next();
-  }*/
+//http://localhost:3000/
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname + '/login.html'));
 });
 
 // http://localhost:3000/home
 app.get('/home', function(req, res) {
-	// If the user is loggedin
-	if (req.session.loggedin) {
-		// Output username
-		res.send('Welcome back, ' + req.session.username + '!');
+	if (!req.session.loggedin) {
+    res.send('Please login to view this page!');
 	} else {
-		// Not logged in
-		res.send('Please login to view this page!');
+	}
+	res.end();
+});
+
+// http://localhost:3000/admin
+app.get('/admin', function(req, res) {
+	if (req.session.username != "admin") {
+    res.send('Please login as an administrator to view this page!');
+	} else {
 	}
 	res.end();
 });
